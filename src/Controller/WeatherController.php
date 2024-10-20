4b70e1b1048ab70752c908c420bd4be40c96d54a
1Batrex1 <bartosz.gajda@protonmail.com>
2024-10-20 18:25:53 +0200

    Lab-f

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Location;
 use App\Repository\LocationRepository;
 use App\Repository\MeasurementRepository;
+use App\Service\WeatherUtil;
+use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,17 +14,15 @@ use Symfony\Component\Routing\Annotation\Route;
 class WeatherController extends AbstractController
 {
     #[Route('/weather/{country}/{city}', name: 'app_weather')]
-    public function city(string $country,string $city,MeasurementRepository $measurementRepository,LocationRepository $locationRepository): Response
+    public function city(
+        #[MapEntity(mapping: ['country' => 'country', 'city' => 'city'])]
+        Location $location,
+        WeatherUtil $weatherUtil,
+    ): Response
     {
 
-        $location = $locationRepository->findByCountryAndCity($country,$city);
 
-        if (!$location) {
-            throw $this->createNotFoundException('The location does not exist');
-        }
-
-
-        $measurements = $measurementRepository->findByLocation($location);
+        $measurements = $weatherUtil->getWeatherForLocation($location);
 
 
         return $this->render('weather/index.html.twig', [
